a1a29488c54f55a227cd795788df866e18627c6d
Layesall <contact@layesall.com>
2022-04-13 01:14:15 +0200

    First Commit: Reorganization of the project

---
+++ README.md
@@ -0,0 +1,2 @@
+# NIKOU - React Starter Kit
+A simple starter kit with necessary dependencies for a React project
---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Routes, Route } from 'react-router-dom';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='app-component'>
+      <Routes>
+        <Route path='/' element={  ''  } />
+      </Routes>
+    </div>
+  );
+}
