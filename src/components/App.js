f2d06de43b1f848d9f87cb0c7b3159f443d51f1f
Layesall <contact@layesall.com>
2022-04-13 02:56:20 +0200

    Notfound component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
+
+// Import of the components
 import Navbar from './Navbar/Navbar';
 import Footer from './Footer/Footer';
-
 import Welcome from './Welcome/Welcome';
+import Notfound from './Notfound/Notfound';
 
 export default function App() {
+
   return (
-    <div className='app-component'>
+    <div className='app-component' >
       <Navbar />
       <Routes>
         <Route path='/' element={  <Welcome />  } />
-        <Route path='*' element={ <Welcome /> } />
+        <Route path='*' element={ <Notfound /> } />
       </Routes>
       <Footer />
     </div>
---
+++ src/components/Notfound/Notfound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Notfound.css'
+
+export default function Notfound() {
+  return (
+    <div className='notfound-component'>
+      <h1>404 | Notfound</h1>
+    </div>
+  );
+}
